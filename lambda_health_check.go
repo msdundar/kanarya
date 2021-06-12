d4335cb50bf2d5e573fe63baad5fd36b7d060b52
M. Serhat Dundar <msdundars@gmail.com>
2021-06-12 22:23:04 +0200

    Add health check function

---
+++ lambda_health_check.go
@@ -0,0 +1,43 @@
+package kanarya
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/lambda"
+)
+
+type HealthCheckResponse struct {
+	StatusCode int64  `json:"statusCode"`
+	Body       string `json:"body"`
+}
+
+func HealthCheck(
+	client *lambda.Lambda,
+	lambda_package LambdaPackage,
+	version string,
+	payload []byte,
+) ([]int64, error) {
+	var lambdaStatusCodes []int64
+	var response HealthCheckResponse
+
+	result, err := client.Invoke(&lambda.InvokeInput{
+		FunctionName: aws.String(lambda_package.Function.Name),
+		Payload:      payload,
+		Qualifier:    aws.String(version),
+	})
+
+	if err != nil {
+		return lambdaStatusCodes, err
+	}
+
+	err = json.Unmarshal(result.Payload, &response)
+
+	if err != nil {
+		return lambdaStatusCodes, err
+	}
+
+	lambdaStatusCodes = append(lambdaStatusCodes, *result.StatusCode, response.StatusCode)
+
+	return lambdaStatusCodes, nil
+}
