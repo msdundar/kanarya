0e0dbb7af25b5e56ca4d71d99c3ca4d6528cc3d4
M. Serhat Dundar <msdundars@gmail.com>
2021-06-13 05:21:34 +0200

    Add initial specs

---
+++ lambda_client_test.go
@@ -0,0 +1,28 @@
+package kanarya
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+// TestLambdaClient initializes a lambda client and then checks return values
+// of LambdaClient function.
+func TestLambdaClient(t *testing.T) {
+	client := LambdaClient(os.Getenv("AWS_REGION"))
+
+	returnValStr := reflect.TypeOf(client).String()
+	expectedReturn := "*lambda.Lambda"
+
+	if returnValStr != expectedReturn {
+		t.Fatalf("LambdaClient() should return %v but returned %v", expectedReturn, returnValStr)
+	}
+
+	field := reflect.TypeOf(*client).Field(0)
+	fieldType := field.Type.String()
+	expectedType := "*client.Client"
+
+	if fieldType != expectedType {
+		t.Fatalf("LambdaClient() should be a type of %v but returned %v", expectedType, fieldType)
+	}
+}
---
+++ s3_client_test.go
@@ -0,0 +1,28 @@
+package kanarya
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+// TestS3Client initializes an S3 client and then checks return values of
+// S3Client function.
+func TestS3Client(t *testing.T) {
+	client := S3Client(os.Getenv("AWS_REGION"))
+
+	returnValStr := reflect.TypeOf(client).String()
+	expectedReturn := "*s3.S3"
+
+	if returnValStr != expectedReturn {
+		t.Fatalf("S3Client() should return %v but returned %v", expectedReturn, returnValStr)
+	}
+
+	field := reflect.TypeOf(*client).Field(0)
+	fieldType := field.Type.String()
+	expectedType := "*client.Client"
+
+	if fieldType != expectedType {
+		t.Fatalf("S3Client() should be a type of %v but returned %v", expectedType, fieldType)
+	}
+}
