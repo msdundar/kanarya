6105ef8c65407c6aa7b5eb40fcb35d6486053f50
M. Serhat Dundar <msdundars@gmail.com>
2021-06-13 05:19:57 +0200

    Make S3 client localstack-compatible in CI

--- s3_client.go
+++ s3_client.go
@@ -1,18 +1,28 @@
 package kanarya
 
 import (
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func S3Client(region string) *s3.S3 {
+	path_style := false
+
+	if os.Getenv("CI") == "true" {
+		path_style = true
+	}
+
 	return s3.New(
 		session.Must(session.NewSessionWithOptions(session.Options{
 			SharedConfigState: session.SharedConfigEnable,
 		})),
 		&aws.Config{
-			Region: aws.String(region),
+			Region:           aws.String(region),
+			Endpoint:         aws.String(os.Getenv("AWS_S3_ENDPOINT")),
+			S3ForcePathStyle: aws.Bool(path_style),
 		},
 	)
 }
